ee360d64f3804b182eba3c03bcdca0f4fd5b983d
Daniel Pham <vominhkhiem212@gmail.com>
2020-09-29 23:44:08 +0700

    feat: add comments

---
+++ src/schema.rs
@@ -0,0 +1,31 @@
+table! {
+    users(id) {
+        id -> Integer,
+        username -> Text,
+    }
+}
+
+table! {
+    posts (id) {
+        id -> Integer,
+        user_id -> Integer,
+        title -> Text,
+        body -> Text,
+        published -> Bool,
+    }
+}
+
+table! {
+    comments (id) {
+        id -> Integer,
+        user_id -> Integer,
+        post_id -> Integer,
+        body -> Text,
+    }
+}
+
+joinable!(comments -> posts (post_id));
+joinable!(comments -> users (user_id));
+joinable!(posts -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(comments, posts, users,);
